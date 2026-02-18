b09f3c32f5a483b662fd7397a0e45ababd5ed849
swathy06 <sujswa@gmail.com>
2026-02-18 15:07:35 +0100

    Folder created

---
+++ template.sh
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 18 14:44:00 2026
+
+@author: rampr
+"""
+mkdir -p src
+mkdir -p research 
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py 
+touch src/prompt.py
+touch .env 
+touch setup.py
+touch app.py
+touch research/trials.ipynb 
+touch requirements.txt
+
+
+echo "Directory and files created successfully!."
